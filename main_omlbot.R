74fdf32b02f11fedb5c87dd83fea7ebe7e2dc0fa
pfistfl <pfistererf@googlemail.com>
2018-09-03 21:36:16 +0200

    Update code and plots

--- main_omlbot.R
+++ main_omlbot.R
@@ -37,7 +37,7 @@ surrogate.mlr.lrn = makeLearner("regr.cubist", committees = 20, extrapolation = 
   # Forward selection ----------------------------------------------------------------------------------
   files = list.files("surrogates")[grep(x = list.files("surrogates"), pattern = "_regr.*_classif")]
   # for(i in c(2)) { # seq_along(learner.names)
-  i = 1
+  i = 6
   catf("Learner: %s", learner.names[i])
   set.seed(199 + i)
 
@@ -108,7 +108,7 @@ surrogate.mlr.lrn = makeLearner("regr.cubist", committees = 20, extrapolation = 
       n = 1)
 
   # Evaluate Package Defaults on OOB-Tasks on OpenML
-  oml.res = foreach(it = rev(seq_len(rin$desc$iters))) %dopar%
+  oml.res = foreach(it = seq_len(rin$desc$iters)) %dopar%
     evalMBOOpenML(
       task.ids = names(surrogates$surrogates[rin$test.inds[[it]]]),
       lrn = makeLearner(gsub(x = learner.names[i], "mlr.", "", fixed = TRUE)),
@@ -116,6 +116,7 @@ surrogate.mlr.lrn = makeLearner("regr.cubist", committees = 20, extrapolation = 
       ps = surrogates$param.set,
       it = it,
       n = 32)
+
 
   # Evaluate against random search on Surrogates
   # Evaluate random search on OOB-Tasks on OpenML
